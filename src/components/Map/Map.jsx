fe15e29299d91cad58b6958b5a46254f6a82ad89
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-08-07 06:20:06 +0300

    Create Map Section

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,29 @@
+import { Box, useMediaQuery } from "@mui/material";
+import GoogleMapReact from "google-map-react";
+
 function Map() {
-  return <h2>Map</h2>;
+  const isMobile = useMediaQuery("(min-width: 600px)");
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <Box
+      sx={{
+        height: "85vh",
+        width: "100%",
+      }}
+    >
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCpvq-eSF6z6GHABfkE0ryGS6XgRESqGQo" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </Box>
+  );
 }
 
 export default Map;
